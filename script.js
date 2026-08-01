59d5f3317d2945d95cb4b89dc117bccb11a479ce
Mohammed Bitar <mohammed.bitar30@gmail.com>
2026-08-01 19:59:28 +0300

    first

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("hello");
+const best = 'worst'; 
+const m = `mohammed is the ${best}`;
+console.log(m);
+let x=5 , z=10;
+console.log(typeof m);
+let isCool = true;
+console.log(typeof isCool);
+const c=10;
+console.log(11>c);
+
+let person ={
+    name: 'mohammed',
+    age: 20,
+    favColor: 'red',
+}
+console.log(typeof person);
+ 
